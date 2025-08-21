fc285424e32a3ab162a264e1d436b87b2e3014af
MuhammedArdaZ <zmuhammedarda@gmail.com>
2025-08-21 11:33:07 +0300

    feat: login-register page and array database

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var upload = multer();
+var session = require("express-session");
+var cookieParser = require("cookie-parser");
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
+app.use(cookieParser());
+app.use(session({
+    secret: "sample-secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+var Users = [];
+
+app.get("/", function(req,res){
+    res.render("main");
+})
+
+app.get("/signup", function (req, res) {
+    res.render("signup");
+});
+
+app.post("/signup", function (req, res) {
+    if (!req.body.id || !req.body.password) {
+        res.status("400");
+        res.send("Id and password can not be empty.");
+    }
+    else {
+        const userFound = Users.find((user) => user.id === req.body.id);
+        if (userFound !== undefined) {
+            return res.send("User already exist!!!");
+        }
+        else {
+            var newUser = { id: req.body.id, password: req.body.password };
+            Users.push(newUser);
+            req.session.user = newUser;
+        }
+    }
+    let userListHtml = `<h2>All users:</h2><ul>`;
+    Users.forEach((user) => {
+        userListHtml += `<li>${user.id}</li>`;
+    })
+    userListHtml += `</ul>`;
+    res.send(userListHtml);
+});
+
+app.get("/login", function(req,res){
+    res.render("login");
+})
+
+app.post("/login", function(req, res){
+    const userFind = Users.find((user) => req.body.id === user.id);
+    if(userFind !== undefined){
+        if(userFind.password === req.body.password){
+            res.send(`Welcome!!!`);
+        }
+        else{
+            res.send("Wrong password.");
+        }
+    }
+    else{
+        res.send("No user found in this id.");
+    }
+})
+
+app.listen(3000); 
